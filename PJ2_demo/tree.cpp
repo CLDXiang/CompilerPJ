38c58fe65a9dfd12f3938c1a092521a0b7cc34ad
CLDXiang <417198670@qq.com>
2019-12-12 21:49:56 +0800

    update tree structure

--- PJ2_demo/tree.cpp
+++ PJ2_demo/tree.cpp
@@ -3,28 +3,36 @@
 #include "tree.h"
 using namespace std;
 
-node_t* create_node(string name, string info = "", double value = .0){
-    node_t* node = new node_t;
-    node->name = name;
-    node->line = 0; // TODO:
-    node->col = 0; // TODO:
-    node->info = info;
-    node->value = value;
-    node->parent = nullptr;
-    return node;
+
+Node* create_node(string name, bool isList = false, string info = "", double value = .0){
+    return new Node(name, isList, info, value);
 }
 
-void node_t::add_child(node_t* child) {
-    childs.push_back(child);
-    child->parent = this;
+void Node::add_childs(initializer_list<Node*> new_childs) {
+    for (auto child: new_childs) {
+        if (child == nullptr)
+            continue; // 跳过空的子节点
+        if (child->isList) {
+            // 子节点是列表节点，将子节点的孩子让渡给当前节点
+            for (auto grandson: child->childs) {
+                childs.push_back(grandson);
+                grandson->parent = this;
+            }
+            // TODO: 可能后面会涉及到传递语义值之类的。 
+        } else {
+            // 子节点不是列表节点，正常操作
+            childs.push_back(child);
+            child->parent = this;
+        }
+    }
 }
 
-void node_t::show(){ 
+void Node::show(){ 
     cout << "{ name: '" << name << "', line: '" << line <<"', col: '"<<col<<"', info: '"<<info<<"', ";
     if (childs.size() != 0) {
         cout << "\nchildren: [" << endl;
-        for (int i = 0; i < childs.size(); i++) {
-            childs[i]->show();
+        for (auto child: childs) {
+            child->show();
             cout << ","; 
         }
         cout << " ]";
@@ -32,26 +40,38 @@ void node_t::show(){
     cout << " }" << endl;
 }
 
+
 // int main() {
-//     node_t* node1 = create_node("identifier", "X");
-//     node1->line = 4;
-//     node1->col = 15;
-//     node_t* node2 = create_node("identifier", "INT");
-//     node1->line = 4;
-//     node1->col = 19;
-//     node_t* node3 = create_node("identifier", "FOO");
-//     node1->line = 4;
-//     node1->col = 13;
-//     node_t* node4 = create_node("component");
-//     node1->line = 4;
-//     node1->col = 27;
-//     node4->add_child(node1);
-//     node4->add_child(node2);
-//     node_t* root = create_node("procedure_decl");
-//     root->line = 9;
-//     root->col = 5;
-//     root->add_child(node3);
-//     root->add_child(node4);
+//     // Node* node1 = create_node("identifier", "X");
+//     // node1->line = 4;
+//     // node1->col = 15;
+//     // Node* node2 = create_node("identifier", "INT");
+//     // node1->line = 4;
+//     // node1->col = 19;
+//     // Node* node3 = create_node("identifier", "FOO");
+//     // node1->line = 4;
+//     // node1->col = 13;
+//     // Node* node4 = create_node("component");
+//     // node1->line = 4;
+//     // node1->col = 27;
+//     // node4->add_childs({node1});
+//     // node4->add_childs({node2});
+//     // Node* root = create_node("procedure_decl");
+//     // root->line = 9;
+//     // root->col = 5;
+//     // root->add_childs({node3, node4});
+
+//     Node* node1 = create_node("d1");
+//     Node* node2 = create_node("d2");
+//     Node* node3 = create_node("d3");
+//     Node* node4 = create_node("d4");
+//     Node* nodee = nullptr;
+//     Node* node5 = create_node("n1", true);
+//     node5->add_childs({node3, nodee});
+//     Node* node6 = create_node("n2", true);
+//     node6->add_childs({node2, node5});
+//     Node* root = create_node("n3", true);
+//     root->add_childs({node1, node6});
 
 //     root->show();
 // }
--- PJ2_demo/tree.h
+++ PJ2_demo/tree.h
@@ -1,18 +1,28 @@
+#ifndef __TREE_H_
+#define __TREE_H_
+
 #include <string>
 #include <vector>
+#include <initializer_list>
 using namespace std;
 
-struct node_t { 
+struct Node { 
     string name;
     unsigned int line, col; // 行列号
     string info;
     double value; // 值
+    bool isList; // 是否是列表节点（type-list）
 
-    vector<node_t*> childs;
-    node_t* parent;
+    vector<Node*> childs;
+    Node* parent;
 
-    void add_child(node_t* child);
+    Node(string name_i, bool is_list = false, string info_i = "", double value_i = .0)
+    : name(name_i), line(0), col(0), info(info_i), value(value_i), isList(is_list), parent(nullptr) {} // TODO: line & col
+
+    void add_childs(initializer_list<Node*> new_childs);
     void show();
 };
 
-node_t* create_node();
+Node* create_node(string name, bool isList, string info , double value);
+
+#endif
